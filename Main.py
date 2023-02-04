5f04097f2397719d8ed54069ab106e7722efd0bc
CarlosJaBr <carjabo12@gmail.com>
2023-02-03 21:19:25 -0500

    start py project

---
+++ Main.py
@@ -0,0 +1,11 @@
+#En este documento .py se iniciará el estudio del lenguaje de programación python. 
+
+lista = [] #Declaración de la lista.
+lista_1 = ["Carlos","Javier","Bolaños","Riascos"] #Declaración lista homogénea.
+lista_2 = ["Carlos",19,2003,"Ing Sis", False] #Declaración de lista  heterogénea.
+print(lista)
+print(lista_1)
+print(lista_2)
+
+
+
