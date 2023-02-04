5aef581f92d4a2164a644ad2396f99385240f1ca
CarlosJaBr <carjabo12@gmail.com>
2023-02-03 21:30:54 -0500

    fixed method

--- Main.py
+++ Main.py
@@ -1,5 +1,11 @@
 #En este documento .py se iniciará el estudio del lenguaje de programación python. 
 
+#*************************
+a:int = 0 #Esta es la forma de declarar variables y su tipo en Python. 
+print(a)
+#*************************
+
+
 lista = [] #Declaración de la lista.
 lista_1 = ["Carlos","Javier","Bolaños","Riascos"] #Declaración lista homogénea.
 lista_2 = ["Carlos",19,2003,"Ing Sis", False] #Declaración de lista  heterogénea.
@@ -9,8 +15,13 @@ print(lista_2)
 
 
 #Métodos sobre listas
+
 print(lista_1.clear()) #El método clear borra todos los elementos de una lista.
-print(len(lista_1)) #
+print("tamaño lista 1 : " + str(len(lista_1))) #El método len retorna el tamaño de una lista.
+
+
+
+
 
 
 
