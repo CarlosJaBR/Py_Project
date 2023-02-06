c93588701084abf8868adab464a382b70991d179
CarlosJaBr <carjabo12@gmail.com>
2023-02-06 17:07:52 -0500

    Object and class has been added

--- Main.py
+++ Main.py
@@ -1,8 +1,12 @@
 #En este documento .py se iniciará el estudio del lenguaje de programación python. 
 
 #*************************
+from Persona import Persona
+
+
 a:int = 0 #Esta es la forma de declarar variables y su tipo en Python. 
 print(a)
+
 #*************************
 
 
@@ -20,6 +24,17 @@ print(lista_1.clear()) #El método clear borra todos los elementos de una lista.
 print("tamaño lista 1 : " + str(len(lista_1))) #El método len retorna el tamaño de una lista.
 
 
+#************ Creación de instancia de la clase persona
+
+persona_1 = Persona("carlos",19,"España")
+print(persona_1.name)
+print(persona_1.age)
+print(persona_1.country)
+
+list_new = list()
+
+list_new.insert(0,persona_1)
+print(len(list_new))
 
 
 
